e4fbda9fcb037303da146caa6a9f5834b712677e
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2024-07-28 16:08:25 +0300

    Add files via upload

---
+++ tp_objectloader/config.lua
@@ -0,0 +1,36 @@
+Config = {}
+
+---------------------------------------------------------------
+--[[ General Settings ]]--
+---------------------------------------------------------------
+
+Config.Locations = {
+
+    { -- TP MyDog ( Fence for the store dog location )
+        Object = 'p_fence02cx',
+
+        Coords = { 
+            x = -1674.8743896484375, y = -339.5870666503906, z = 172.87277221679688, 
+            pitch = -6.19514608383178, roll = 4.9673957824707, yaw = 123.85027313232422 
+        },
+
+        PlaceObjectOnGroundProperly = false,
+
+        ObjectRenderDistance = 30,
+    },
+
+    { -- TP MyDog ( Fence for the store dog location )
+        Object = 'p_fence02cx',
+
+        Coords = { 
+            x = -1675.1510009765625, y = -340.6896667480469, z = 172.89312744140625, 
+            pitch = -1.4711765050888, roll = -6.58002567291259, yaw = 32.7867317199707
+        },
+
+        PlaceObjectOnGroundProperly = false,
+
+        ObjectRenderDistance = 30,
+    },
+
+}
+
